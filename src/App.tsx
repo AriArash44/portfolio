8ae3da137685727536761f2ddf731ab55035ba63
Arash44 <arashasghari1245@gmail.com>
2025-10-04 21:34:50 +0330

    add type animation for name in header

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Typewriter } from "react-simple-typewriter";
 import PizzaBackground from "./components/PizzaBackground";
 import PizzaIcon from "/icons/pizza.svg";
 
@@ -15,8 +16,19 @@ export default function App() {
       </div>
       <div className="flex flex-col-reverse justify-center items-center sm:flex-row sm:gap-6
       absolute top-16 sm:top-[50px] w-full sm:w-auto px-12 pointer-events-none">
-        <img className="w-20 h-20 mt-4 sm:mt-0" src="/images/profile.svg" alt="Arash Asghari" />
-        <h1 className="font-bold text-custom-gold" style={{fontFamily: 'caveat'}}>Arash Asghari</h1>
+        <img className="w-20 h-20 mt-4 sm:mt-0 rounded-full" style={{boxShadow: "0 0 20px rgba(0,0,0,0.4)"}}
+          src="/images/profile.svg" alt="Arash Asghari" />
+        <h1 className="font-bold text-custom-gold" style={{fontFamily: 'caveat'}}>&nbsp;
+          <Typewriter
+            words={["Arash Asghari"]}
+            loop={0}
+            cursor
+            cursorStyle=""
+            typeSpeed={120}
+            deleteSpeed={50}
+            delaySpeed={1000}
+          />
+        </h1>
       </div>
     </header>
   );
